abacb4b2fbae985612eb5a2a4612343ac1f9a53c
Shreyas Srinivasa <58994291+sastry17@users.noreply.github.com>
2023-07-07 15:50:25 +0200

    css change
    css commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './Selene-logo-nobg.png';
 import './App.css';
 
 function App() {
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://github.com/SeleneCTI"
