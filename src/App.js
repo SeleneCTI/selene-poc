ef8674b8b07acf0d485d8af25c0acad7522caec7
Shreyas Srinivasa <58994291+sastry17@users.noreply.github.com>
2023-07-07 14:26:16 +0200

    css changes

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/SeleneCTI"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Selene CTI - Coming Soon
         </a>
       </header>
     </div>
